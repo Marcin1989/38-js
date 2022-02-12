e47bfa98a4d604546e3fb5693d639eef2ec25fed
Marcin1989 <87943126+Marcin1989@users.noreply.github.com>
2022-02-12 11:24:45 +0100

    change

--- js/main.js
+++ js/main.js
@@ -3,5 +3,5 @@ let change = document.getElementById("opis");
 let press = document.querySelector(`button`);
 
 press.onclick = function () {
-    change.textContent("Napisalem tu cos");
+    change.textContent = "Napisalem tu cos";
 }
